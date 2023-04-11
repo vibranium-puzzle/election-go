d0fb11338c38165509a31869dcdbb9531f83751a
ZhangJian He <shoothzj@gmail.com>
2023-04-11 19:23:23 +0800

    support custom float ip command

--- election/float_ip_config.go
+++ election/float_ip_config.go
@@ -7,16 +7,29 @@ type FloatIpConfig struct {
 	VirtualIp   string
 	NetworkMask string
 	Gateway     string
+
+	CommandIpUp   string
+	CommandIpDown string
+	CommandArp    string
 }
 
 func (f *FloatIpConfig) UpCmd() string {
+	if f.CommandIpUp != "" {
+		return f.CommandIpUp
+	}
 	return "/sbin/ifconfig " + f.VirtualDev + " " + f.VirtualIp + " netmask " + f.NetworkMask + " up"
 }
 
 func (f *FloatIpConfig) DownCmd() string {
+	if f.CommandIpDown != "" {
+		return f.CommandIpDown
+	}
 	return "/sbin/ifconfig " + f.VirtualDev + " " + f.VirtualIp + " netmask " + f.NetworkMask + " down"
 }
 
 func (f *FloatIpConfig) ArpCmd() string {
+	if f.CommandArp != "" {
+		return f.CommandArp
+	}
 	return "/sbin/arping -I " + f.Dev + " -w 0 -c 1 -U -s " + f.VirtualIp + " " + f.VirtualIp
 }
